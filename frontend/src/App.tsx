d4ee1de26e494f668267463ceadd19d72e6e601b
Akshit Batra <akshit.batra@thoughtworks.com>
2025-10-31 22:30:14 +0530

    feat(ui): refactoring routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -17,25 +17,20 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/admin" element={<AdminPage />} />
-        <Route path="/admin/users" element={<AdminUserListPage />} />
-        <Route path="/admin/content-types" element={<AdminContentTypesPage />} />
-        <Route path="/admin/entries" element={<AdminEntriesPage />} />
-        <Route path="/admin/media" element={<AdminMediaPage />} />
-        <Route path="/admin/settings" element={<AdminSettingsPage />} />
-        <Route path="/admin/roles" element={<AdminRolesPage />} />
-        <Route path="/admin/logs" element={<AdminLogsPage />} />
-        <Route path="/admin/profile" element={<AdminProfilePage />} />
-        <Route
-          path="*"
-          element={
-            <Routes>
-              <Route path="/" element={<LandingPage />} />
-              <Route path="/docs" element={<Documentation />} />
-              {/* Add more non-admin routes here */}
-            </Routes>
-          }
-        />
+        <Route path="/admin">
+          <Route index element={<AdminPage />} />
+          <Route path="users" element={<AdminUserListPage />} />
+          <Route path="content-types" element={<AdminContentTypesPage />} />
+          <Route path="entries" element={<AdminEntriesPage />} />
+          <Route path="media" element={<AdminMediaPage />} />
+          <Route path="settings" element={<AdminSettingsPage />} />
+          <Route path="roles" element={<AdminRolesPage />} />
+          <Route path="logs" element={<AdminLogsPage />} />
+          <Route path="profile" element={<AdminProfilePage />} />
+        </Route>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/docs" element={<Documentation />} />
+        {/* Add more non-admin routes here */}
       </Routes>
     </BrowserRouter>
   );
